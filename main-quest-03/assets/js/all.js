695ba75dea98df6a95a1429fe124b5eb9b899d7b
yiling9incode <yiling9incode@gmail.com>
2026-08-16 03:35:48 +0800

    新增main-quest-03
    新增main-quest-03

---
+++ main-quest-03/assets/js/all.js
@@ -0,0 +1,42 @@
+const swiper = new Swiper('.swiper', {
+
+    // 分頁、左右箭頭、滾動條若有使用則必需設定
+    // 分頁
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    // 左右箭頭
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // 滾動條
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+const bestSellerSwiper = new Swiper(".best-seller-swiper", {
+  slidesPerView: 1.2,
+  spaceBetween: 16,
+
+  breakpoints: {
+    577: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+  },
+});
+
+function updateBestSellerSwiper() {
+  if (window.innerWidth >= 577) {
+    bestSellerSwiper.slideTo(0, 0);
+    bestSellerSwiper.disable();
+  } else {
+    bestSellerSwiper.enable();
+  }
+}
+
+updateBestSellerSwiper();
+
+window.addEventListener("resize", updateBestSellerSwiper);
