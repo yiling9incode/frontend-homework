749673a9052320d9286e89ca92bc45895eb1f22f
yiling9incode <yiling9incode@gmail.com>
2026-08-21 19:13:28 +0800

    新增main-quest-05

---
+++ main-quest-05/main.js
@@ -0,0 +1,19 @@
+// import './assets/scss/all.scss';
+
+// console.log('Hello world');
+
+
+// Bootstrap 原始樣式
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// 自己的 SCSS，放在 Bootstrap 後面才能覆蓋樣式
+import './assets/scss/all.scss';
+
+// Bootstrap 的互動功能：Offcanvas、Collapse 等
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// 自己的 JavaScript
+import './assets/js/main.js';
